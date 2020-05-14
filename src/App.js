ff96fa2d0701b62b70d73566ad76e511a89256b5
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-14 15:08:49 -0300

    Initial commit with setup

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Github - Explore</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: './index.html',
+    }),
+  ],
+};
