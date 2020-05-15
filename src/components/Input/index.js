c73d2a561e787936e7df8694310a353f5719c944
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-15 11:58:43 -0300

    Implement Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  border-radius: 0.3rem;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  font-size: 1.6rem;
+`;
