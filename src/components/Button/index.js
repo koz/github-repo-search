5ab3d3795d7096799041fcea6b00a2bc647026ca
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-15 11:58:31 -0300

    Implement button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background-color: black;
+  color: white;
+  padding: 1rem;
+  border-radius: 0.3rem;
+  boder: none;
+  font-size: 1.6rem;
+`;
