c8d488a9f09ce0f24a32909ef373da961a72bcf2
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-17 18:11:30 -0300

    Add Provider to Routes test file

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { createMemoryHistory } from 'history';
 import Routes from './Routes';
 import { render } from '@testing-library/react';
+import createStore from './redux/createStore';
 
 describe('<Routes />', () => {
   test('should show different content after navigating to details route', () => {
     const history = createMemoryHistory();
     const { container } = render(
-      <Router history={history}>
-        <Routes />
-      </Router>
+      <Provider store={createStore()}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </Provider>
     );
     const homeText = container.textContent;
     history.push('/123');
