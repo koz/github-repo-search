f26e7a93dd30e5c47025792bff1c53c2749a83b7
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-17 18:10:26 -0300

    Implement reducer initial logic

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,25 @@
-export default (state) => state;
+import { FETCH_REPOSITORIES_START, FETCH_REPOSITORIES_ERROR, FETCH_REPOSITORIES_SUCCESS } from '../actions/actions';
+
+export default (state, action) => {
+  switch (action.type) {
+    case FETCH_REPOSITORIES_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_REPOSITORIES_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    case FETCH_REPOSITORIES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        repositories: action.payload,
+      };
+    default:
+      return state;
+  }
+};
