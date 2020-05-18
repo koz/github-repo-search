1826d93fdec3732326f09f3cc3aa6d009368c241
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-18 17:41:06 -0300

    Implement useRepositories selector

--- src/redux/selectors/selectors.test.js
+++ src/redux/selectors/selectors.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { renderWithProvider } from '../../../spec/utils/renderWithContext';
-import { useTotalCount } from '.';
+import { useTotalCount, useRepositories } from '.';
 
 const setup = (selector, state) => {
   let val = {};
@@ -16,5 +16,10 @@ describe('selectors', () => {
   test('useTotalCount', () => {
     const value = setup(useTotalCount, { repositories: { totalCount: 10000 } });
     expect(value).toBe(10000);
+  });
+
+  test('useRepositories', () => {
+    const value = setup(useRepositories, { repositories: { items: [1, 2, 3] } });
+    expect(value).toStrictEqual([1, 2, 3]);
   });
 });
