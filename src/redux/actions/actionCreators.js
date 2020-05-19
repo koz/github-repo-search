e77553212a750dc790461e3496c8dc96b79c776f
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-19 16:02:27 -0300

    Implement new actions for fetching a single repo

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,4 +1,11 @@
-import { FETCH_REPOSITORIES_START, FETCH_REPOSITORIES_SUCCESS, FETCH_REPOSITORIES_ERROR } from './actions';
+import {
+  FETCH_REPOSITORIES_START,
+  FETCH_REPOSITORIES_SUCCESS,
+  FETCH_REPOSITORIES_ERROR,
+  FETCH_REPOSITORY_START,
+  FETCH_REPOSITORY_SUCCESS,
+  FETCH_REPOSITORY_ERROR,
+} from './actions';
 
 export const fetchRepositoriesStart = () => ({
   type: FETCH_REPOSITORIES_START,
@@ -11,5 +18,19 @@ export const fetchRepositoriesSuccess = (data) => ({
 
 export const fetchRepositoriesError = (error) => ({
   type: FETCH_REPOSITORIES_ERROR,
+  payload: error,
+});
+
+export const fetchRepositoryStart = () => ({
+  type: FETCH_REPOSITORY_START,
+});
+
+export const fetchRepositorySuccess = (data) => ({
+  type: FETCH_REPOSITORY_SUCCESS,
+  payload: data,
+});
+
+export const fetchRepositoryError = (error) => ({
+  type: FETCH_REPOSITORY_ERROR,
   payload: error,
 });
--- src/redux/actions/actionCreators.test.js
+++ src/redux/actions/actionCreators.test.js
@@ -1,5 +1,19 @@
-import { fetchRepositoriesStart, fetchRepositoriesSuccess, fetchRepositoriesError } from './actionCreators';
-import { FETCH_REPOSITORIES_START, FETCH_REPOSITORIES_SUCCESS, FETCH_REPOSITORIES_ERROR } from './actions';
+import {
+  fetchRepositoriesStart,
+  fetchRepositoriesSuccess,
+  fetchRepositoriesError,
+  fetchRepositoryStart,
+  fetchRepositorySuccess,
+  fetchRepositoryError,
+} from './actionCreators';
+import {
+  FETCH_REPOSITORIES_START,
+  FETCH_REPOSITORIES_SUCCESS,
+  FETCH_REPOSITORIES_ERROR,
+  FETCH_REPOSITORY_START,
+  FETCH_REPOSITORY_SUCCESS,
+  FETCH_REPOSITORY_ERROR,
+} from './actions';
 
 describe('actionCreators', () => {
   describe('fetchRepositories', () => {
@@ -27,6 +41,34 @@ describe('actionCreators', () => {
     test('should return the param as payload', () => {
       const error = { code: 111 };
       expect(fetchRepositoriesError(error)).toMatchObject({ type: FETCH_REPOSITORIES_ERROR, payload: error });
+    });
+  });
+
+  describe('fetchRepository', () => {
+    test('should return the correct type', () => {
+      expect(fetchRepositoryStart()).toMatchObject({ type: FETCH_REPOSITORY_START });
+    });
+  });
+
+  describe('fetchRepositorySuccess', () => {
+    test('should return the correct type', () => {
+      expect(fetchRepositorySuccess()).toMatchObject({ type: FETCH_REPOSITORY_SUCCESS });
+    });
+
+    test('should return the param as payload', () => {
+      const data = { a: 1 };
+      expect(fetchRepositorySuccess(data)).toMatchObject({ type: FETCH_REPOSITORY_SUCCESS, payload: data });
+    });
+  });
+
+  describe('fetchRepositoriesError', () => {
+    test('should return the correct type', () => {
+      expect(fetchRepositoryError()).toMatchObject({ type: FETCH_REPOSITORY_ERROR });
+    });
+
+    test('should return the param as payload', () => {
+      const error = { code: 111 };
+      expect(fetchRepositoryError(error)).toMatchObject({ type: FETCH_REPOSITORY_ERROR, payload: error });
     });
   });
 });
