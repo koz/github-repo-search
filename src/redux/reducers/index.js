00d442cd8a60f13a4d309b66a125b81e89fe86fe
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-21 11:29:54 -0300

    Implement readmeFiles reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import repositories from './repositories';
 import owners from './owners';
+import readmeFiles from './readmeFiles';
 
 export default combineReducers({
   repositories,
   owners,
+  readmeFiles,
 });
