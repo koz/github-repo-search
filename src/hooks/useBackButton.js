8a1df582ed35b20eb289afd98cb59bebf076a34a
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-28 02:01:30 -0300

    Change useBackButton history length to useRef

--- src/hooks/useBackButton.js
+++ src/hooks/useBackButton.js
@@ -1,15 +1,15 @@
 import { useLocation, useHistory } from 'react-router-dom';
-import { useState, useMemo, useEffect } from 'react';
+import { useState, useMemo, useEffect, useRef } from 'react';
 
 export default () => {
   const location = useLocation();
   const history = useHistory();
   const [showBack, setShowBack] = useState(false);
   const isHomePage = useMemo(() => !!location.pathname.match(/^\/$/), [location.pathname]);
-  const initialHistoryLength = useMemo(() => history.length, []);
+  const initialHistoryLength = useRef(history.length);
   useEffect(() => {
-    setShowBack(!isHomePage && history.length > initialHistoryLength);
-  }, [isHomePage, initialHistoryLength, history.length]);
+    setShowBack(!isHomePage && history.length > initialHistoryLength.current);
+  }, [isHomePage, history.length]);
   const handleClick = () => history.goBack();
   return {
     state: showBack,
